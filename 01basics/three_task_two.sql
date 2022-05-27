a228dee2bb9dc647ba9d0b4c93bbda8a7fc63b80
unknown <kapil.nargund.kn@gmail.com>
2022-05-27 18:50:26 +0530

    groupby hands on

--- 01basics/three_task_two.sql
+++ 01basics/three_task_two.sql
@@ -23,5 +23,41 @@
 -- SELECT stu_fname, signup_month, COUNT(*) AS count_of_stu_signed_up_on_same_month FROM students
 -- GROUP BY signup_month;
 
+-- MIN MAX AND SUBQUERIES
 -- SELECT stu_fname, login_count FROM students WHERE login_count = (SELECT MAX(login_count) FROM students);
-SELECT stu_fname, course_count FROM students WHERE course_count = (SELECT MIN(course_count) FROM students);
+-- SELECT stu_fname, course_count FROM students WHERE course_count = (SELECT MIN(course_count) FROM students);
+
+-- GROUP BY WITH MIN MAX
+-- SELECT stu_fname, signup_month, MIN(login_count), COUNT(*) FROM students GROUP BY signup_month;
+
+
+-- SUM AND AVERAGE WITH GROUP BY
+-- SELECT stu_fname, SUM(login_count), AVG(course_count), signup_month FROM students GROUP BY signup_month;
+
+-- AND OR
+
+-- SELECT stu_fname, login_count, course_count FROM students WHERE login_count >= 20 AND course_count >= 5;
+-- SELECT stu_fname, signup_month FROM students WHERE signup_month = 3 OR signup_month = 7 ORDER BY signup_month;
+
+-- RANGE (KEYWORDS : 'BETWEEN', 'AND')
+-- SELECT stu_fname, signup_month FROM students WHERE signup_month BETWEEN 3 AND 7 ORDER BY signup_month;
+
+-- CASE (CONDITIONAL STATEMENTS)
+-- SELECT stu_fname, signup_month,
+--     CASE
+--         WHEN signup_month BETWEEN 3 and 7 THEN 'EARLY BIRDS'
+--         WHEN signup_month BETWEEN 8 and 12 THEN 'NORMAL USERS'
+--         ELSE 'NO DISCOUNT USERS'
+--     END AS USER_CATEGORY
+-- FROM students;
+
+-- TO SAVE A RESULTANT TABLE
+-- CREATE TABLE case_result_table 
+-- SELECT stu_fname, signup_month,
+--     CASE
+--         WHEN signup_month BETWEEN 3 and 7 THEN 'EARLY BIRDS'
+--         WHEN signup_month BETWEEN 8 and 12 THEN 'NORMAL USERS'
+--         ELSE 'NO DISCOUNT USERS'
+--     END AS USER_CATEGORY
+-- FROM students;
+
